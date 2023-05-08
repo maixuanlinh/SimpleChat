e3d1a3ef1cac77877210723cc1aa136e6915716c
Mai Xuan Linh <maixuanlinhdkh@gmail.com>
2023-05-08 14:00:05 +0300

    chat app first completed

---
+++ public/chat.js
@@ -0,0 +1,18 @@
+let socket = io.connect("http://localhost:3000");
+
+var message = document.getElementById("message");
+var button = document.getElementById("send");
+var username = document.getElementById("username");
+var output = document.getElementById("output")
+
+button.addEventListener("click", function () {
+    socket.emit("sendingMessage", {
+        message: message.messageValue,
+        username: username.value,
+    });
+});
+
+socket.on("broadcastMessage", function (data) {
+    output.innerHTML += 
+    "<p><strong>" + data.username + ": </strong>" + data.message + "</p>";
+});
